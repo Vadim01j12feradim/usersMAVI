a20661b04036c2d19b1b9cb226027f3807aa0326
Vadim01j12feradim <izmael.guzman7281@alumnos.udg.mx>
2024-03-05 14:45:17 -0600

    asd

--- addUser.php
+++ addUser.php
@@ -6,7 +6,7 @@ require 'dashboard/sessionValidation.php';
 <html lang="en" >
 <head>
   <meta charset="UTF-8">
-  <title>CodePen - Contact us Form</title>
+  <title>Add user</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
 <link rel="stylesheet" href="./style.css">
 <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script> -->
--- dashboard/index.php
+++ dashboard/index.php
@@ -5,7 +5,7 @@ require 'sessionValidation.php';
 <!doctype html>
 <html lang="en">
   <head>
-  	<title>Table 09</title>
+  	<title>Users</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
